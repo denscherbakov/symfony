cd406525aab4e605c7aa0925084127eb1eb5b95f
Denis Shcherbakov <denscherbakov@Air-Denis.Dlink>
2021-04-16 13:35:26 +0700

    add admin panel and auth

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AdminUserController extends AdminBaseController
+{
+    /**
+     * @Route("/admin/users", name="admin/users_list")
+     */
+    public function index()
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        $forRender = parent::renderDefault();
+        $forRender['users'] = $users;
+        return $this->render('admin/user/index.html.twig', $forRender);
+    }
+
+    /**
+     * @Route("/admin/users/create", name="admin/users/create")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return RedirectResponse|Response
+     */
+    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $em = $this->getDoctrine()->getManager();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_ADMIN']);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin/users_list');
+        }
+
+        $forRender = parent::renderDefault();
+        $forRender['title'] = 'Create user';
+        $forRender['form'] = $form->createView();
+
+        return $this->render('admin/user/form.html.twig', $forRender);
+
+    }
+}
